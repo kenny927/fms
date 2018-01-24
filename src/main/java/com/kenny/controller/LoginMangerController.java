186f43483ae42bf8fcbdef1a18171a29d69f3ea6
kenny927 <zhangming927@whu.edu.cn>
2018-01-24 21:06:26 +0800

    create fms

---
+++ src/main/java/com/kenny/controller/LoginMangerController.java
@@ -0,0 +1,45 @@
+package com.kenny.controller;
+
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ *
+ * 订单平台登录模块
+ *
+ * @author kewei
+ *
+ */
+@Controller
+@RequestMapping("/login/")
+public class LoginMangerController {
+
+
+    private static final Logger log = Logger.getLogger(LoginMangerController.class);
+
+
+    /**
+     * 系统登录页面
+     */
+    private static final String preLogin = "/pages/login/login";
+
+
+
+    @RequestMapping("preLogin")
+    public String preLogin(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Map<String, Object> loginResult = new HashMap<String, Object>();
+        loginResult.put(Constant.MSG.toString(), "");
+        request.setAttribute("loginResult", loginResult);
+        return preLogin;
+    }
+
+}
